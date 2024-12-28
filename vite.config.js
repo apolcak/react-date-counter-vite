6dee9620db5feedc85336d60fc4a28c9d32b2cd5
Antonín Polčák <antoninpolcak@gmail.com>
2024-12-28 14:01:11 +0100

    Vite config update to use .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+console.log("VITE_BASE_PATH:", process.env.VITE_BASE_PATH); // Debugging line
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: process.env.VITE_BASE_PATH || "/",
+});
